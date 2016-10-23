aebc5df6115c9de64a8aa3ac3544b9663edc9182
brand <brand@DESKTOP-NDVG4BB>
2016-10-23 15:22:35 -0700

    First commit to Audiobook uwp.  Plays hard coded song and displays fake cover art.

---
+++ AudioBook/Audiobook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+
+namespace AudioBook
+{
+    class Audiobook
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Narrator { get; set; }
+        public TimeSpan TotalTime { get; set; }
+        private MediaPlaybackList trackList;
+
+        public Audiobook(MediaPlaybackList tracks)
+        {
+            trackList = tracks;
+            MediaItemDisplayProperties displayProps = trackList.CurrentItem.GetDisplayProperties();
+            if (MediaPlaybackType.Music == displayProps.Type)
+            {
+                Title = displayProps.MusicProperties.AlbumTitle;
+                Author = displayProps.MusicProperties.AlbumArtist;
+                while (null != trackList.CurrentItem)
+                {
+                    TotalTime.Add(trackList.CurrentItem.DurationLimit.Value);
+                    trackList.MoveNext();
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ AudioBook/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AudioBook
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            coverArt.Source = new BitmapImage(new Uri("ms-appx:///Assets/Images/The_Fifth_Season.jpg"));
+            mpe.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Assets/Audio/Terra_in_Black.mp3"));
+        }
+    }
+}
